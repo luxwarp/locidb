52e0ecb1504a4e5a29026068e9688590685c7689
Mikael Carlsson <mikael@codeanarc.com>
2017-04-12 21:18:30 +0200

    Added: 	listTables() for listing all created tables 	drop() for removing all tables in the folder where it saved. 		Use with caution because it will delete all 		files in that folder. Fix: Code cleanup and renaming some variables.

--- index.js
+++ index.js
@@ -4,61 +4,90 @@
 var fs = require("fs");
 
 const pathToDB = "lociDB/";
-const tableExtension = ".db";
+const tblExtension = ".db";
 
 class Loci {
+    /**
+     * Constructor will create a folder for all table files.
+     */
     constructor() {
-        
+        //Lets create a folder if it does not exist when we starting to use the module.
         if(!fs.existsSync(pathToDB)) {
             fs.mkdirSync(pathToDB);
         }
     }
 
-    
     /**
      * This inserts value to last row in the table. 
-     * @param {String} dbName The name off the table for writing the value to. 
-     * @param {Object} value Needs to be an object. No need to stringify first we do that FOR YOU.
+     * @param {String} tblName The name off the table for writing the value to. 
+     * @param {Object} value Needs to be an object. No need to stringify first, LOCI DO THAT FOR YOU!
      */
-    insert(dbName, value) {
+    insert(tblName, value) {
         var existing;
         
-        if(fs.existsSync(pathToDB+dbName+tableExtension)) {
-            if(fs.statSync(pathToDB+dbName+tableExtension).size == 0) {
+        if(fs.existsSync(pathToDB+tblName+tblExtension)) {
+            if(fs.statSync(pathToDB+tblName+tblExtension).size == 0) {
                 existing = [];
             }else {
-                existing = JSON.parse(fs.readFileSync(pathToDB+dbName+tableExtension,"utf8"));
+                existing = JSON.parse(fs.readFileSync(pathToDB+tblName+tblExtension,"utf8"));
             }
         }else {
             existing = [];
         }
 
-        existing.push(value)
+        existing.push(value);
 
-        fs.writeFileSync(pathToDB+dbName+tableExtension, JSON.stringify(existing,null,"\t"));
+        fs.writeFileSync(pathToDB+tblName+tblExtension, JSON.stringify(existing,null,"\t"));
     }
     
     /**
-     * This will return all from the table.
-     * @param {String} dbName Name of the table. 
+     * This will return all rows from the table as an array of objects.
+     * @param {String} tblName Name of the table to return. 
      */
-    get(dbName) {
+    get(tblName) {
         var existing;
         
-        if(fs.existsSync(pathToDB+dbName+tableExtension)) {
-            if(fs.statSync(pathToDB+dbName+tableExtension).size == 0) {
+        if(fs.existsSync(pathToDB+tblName+tblExtension)) {
+            if(fs.statSync(pathToDB+tblName+tblExtension).size == 0) {
                 existing = [];
                 return existing;
             }else {
-                existing = JSON.parse(fs.readFileSync(pathToDB+dbName+tableExtension,"utf8"));
+                existing = JSON.parse(fs.readFileSync(pathToDB+tblName+tblExtension,"utf8"));
                 return existing;
             }
-        } else {
-            return "Error: table not found.";
+        }else {
+            return "Error: Table "+ tblName +" not found.";
         }
     }
-    
-}
 
+    /**
+     * This will return all tables that exists as an array of strings with file extension.
+     */
+    listTables() {
+        var files = fs.readdirSync(pathToDB);
+        
+        if(files.length <= 0) {
+            return "Error: No tables found in folder " + pathToDB;
+        }else {
+            return files;
+        }
+    }
+
+    /**
+     * Drop/Deletes all tables (ALL FILES IN THE LOCIDB DIRECTORY), ONLY USE IF YOU KNOW WHAT YOU DOING.
+     */
+    drop() {
+        var files = fs.readdirSync(pathToDB);
+        
+        if(files.length <= 0) {
+            return "Error: No tables found in folder " + pathToDB;
+        }else {
+            for(var i = 0; i < files.length; i++) {
+                fs.unlinkSync(pathToDB+files[i]);
+            }
+            return true;
+        }
+    }
+}
 
 module.exports = Loci;
--- test.js
+++ test.js
@@ -1,17 +1,21 @@
 /* this file is for testing the module. run it the terminal with "node ./test" */
 
-var Loci = require("./index");
+var Loci = require("./index"); //import the module.
+var db = new Loci(); //create an instance of loci.
 
-var db = new Loci();
-
-var user = { 
+var user = {                //this is an example object that we will insert into a table.
     name: "mikael",
     age: 27,
     city: "Bohus",
     country: "Sweden"
 };
 
-db.insert("users", user);
-var users = db.get('users');
+console.log(db.listTables());   //First list tables to se if it exists any already.
 
- console.log(users);
+db.insert("users", user);       //insert the object to the table at the end.
+
+var users = db.get("users");    //get all rows in the table as an array of objects
+
+console.log(users);             //Print out the array so we can watch it.
+
+//db.drop();                      //Drop/deletes all tables. Only use if you know what you doing.
